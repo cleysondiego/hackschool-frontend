475c1509da7edc5458af1ca95467651be5110699
Cleyson Diego <cleyson7@hotmail.com>
2019-11-27 01:45:26 -0300

    Iniciando estilizaçao dos eventos.

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { Container, EventsList, Event } from './styles';
+import { Container, EventsList, Event, Title, EventDate } from './styles';
 
 export default function Events() {
   return (
     <Container>
       <center>
-        <h1>Events</h1>
+        <h1>Eventos</h1>
       </center>
 
       <EventsList>
         <Event>
-          <Link to="/">
+          <a
+            href="https://rocketseat.com.br/experience"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               src="https://rocketseat.com.br/static/images/logo-rocketseat.svg"
               alt=""
             />
             <div>
-              <h1>RS/XP</h1>
+              <EventDate>25 à 27 de Novembro</EventDate>
+              <Title>RS/XP</Title>
               <p>
-                Essa é uma descrição muita massa desse curso que eu
+                Essa é uma descrição muita massa desse evento que eu
                 particulamente já considero pakas
               </p>
+              <small>São Paulo Expo Center</small>
             </div>
-          </Link>
+          </a>
         </Event>
       </EventsList>
     </Container>
--- src/pages/Events/styles.js
+++ src/pages/Events/styles.js
@@ -10,6 +10,49 @@ export const Container = styled.div`
   }
 `;
 
-export const EventsList = styled.ul``;
+export const EventsList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const Event = styled.li``;
+export const Event = styled.li`
+  & > a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px 0;
+    color: #fff;
+    width: 40%;
+    height: 300px;
+    border: 1px solid #ccc;
+    background: #1c294a;
+    border-radius: 6px;
+    padding: 10px;
+
+    & > img {
+      height: 100px;
+    }
+
+    & > div {
+      text-align: center;
+      justify-content: space-between;
+      padding: 10px;
+      height: 70%;
+
+      * {
+        margin-top: 10px;
+      }
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 30px;
+`;
+
+export const EventDate = styled.p`
+  font-size: 12px;
+  margin-top: 10px;
+`;
